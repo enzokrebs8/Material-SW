4b01d7a2e2881f84c83afd306d4dc38cb26f3634
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-02-29 12:44:40 -0300

    Add files via upload

---
+++ Aula 02 de SW/ExemploLampada/Lampada/Lampada/Lampada.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada_Aula_01
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+        public void ligar()
+        {
+            Ligada = true;
+        }
+        public void desligar()
+        {
+            Ligada = false;
+        }
+        
+        public bool estaLigada()
+        {
+            return Ligada;
+        }
+        public void cores()
+        {
+            Cor = "Vermelha";
+        }
+
+        public string qualCor()
+        {
+            return Cor;
+        }
+        public void potencia()
+        {
+            Potencia = 100;
+        }
+        public int qualPot()
+        {
+            return Potencia;
+        }
+
+    }
+}
---
+++ Aula 02 de SW/ExemploLampada/Lampada/Lampada/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lampada_Aula_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //instancia de um objeto:
+            Lampada lampada1 = new Lampada();
+
+            lampada1.ligar();
+            lampada1.desligar();
+            lampada1.cores();
+            lampada1.potencia();
+
+            Console.WriteLine("A lâmapada está ligada? " + lampada1.estaLigada());
+            Console.WriteLine("Qual é a cor da lâmpada? " + lampada1.qualCor());
+            Console.WriteLine("Qual é a potência da lâmpada? " + lampada1.qualPot());
+        }
+    }
+}
