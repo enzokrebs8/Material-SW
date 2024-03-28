a9c5dcdd052050485824588149a91ab2a8426f40
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-03-27 22:00:41 -0300

    Add files via upload

---
+++ Aula 03 e 04/Banco/Banco/Banco/Conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        private string? agencia { get; set; }
+        private string? usuário { get; set; }
+        private float saldo { get; set; }
+        private float limite { get; set; }
+
+        public void bradesco(string usuário)
+        {
+            agencia = "Bradesco";
+            Console.WriteLine("Você faz parte da agência: " + agencia + ". E seu id é: " + usuário);
+        }
+
+        public void sacar(float valorsaque)
+        {
+            if (valorsaque > saldo + limite)
+            {
+                Console.WriteLine("Você só pode sacar até R$:" + (saldo + limite));
+            }
+            else
+            {
+                saldo -= valorsaque;
+                Console.WriteLine("Você fez um saque de R$:" + valorsaque + " seu saldo agora é de R$:" + saldo);
+            }
+        }
+
+        public void depositar(float valordeposito)
+        {
+            saldo = saldo + valordeposito;
+            Console.WriteLine("Você fez um depósito de R$:" + valordeposito + " seu saldo agora é de R$:" + saldo);
+        }
+
+        public float versaldo()
+        {
+            return saldo;
+        }
+
+        public float verlimite()
+        {
+            return limite;
+        }
+
+        public void mudarlimite(float maxlimite)
+        {
+            limite = maxlimite;
+            Console.WriteLine("Você mudou seu limite para R$:" + limite);
+        }
+    }
+}
---
+++ Aula 03 e 04/Banco/Banco/Banco/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            conta.depositar(500); //valor inicial
+            conta.mudarlimite(900);
+            Console.WriteLine("você fez um depósito e está com o saldo de R$:" + conta.versaldo()); //mostra saldo
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            conta.bradesco("Davi Antonio Ramos Araujo");
+            Console.WriteLine();
+            Console.WriteLine("Seu saldo é de R$" + conta.versaldo() + " e seu limite é de R$:" + conta.verlimite()); //inicio
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            conta.sacar(2000); //saque inválido
+            conta.mudarlimite(1500); //mudar limite
+            conta.sacar(2000); //saque
+            conta.depositar(2500); //depositar
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Seu saldo é de R$" + conta.versaldo() + " e seu limite é de R$:" + conta.verlimite()); //ver saldo
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
