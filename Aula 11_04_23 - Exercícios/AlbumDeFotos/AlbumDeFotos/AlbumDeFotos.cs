31b74e27913a5325cda70ff177d64cbb91ca3a4f
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-04-12 23:40:55 -0300

    Add files via upload

---
+++ Aula 11_04_23 - Exercícios/AlbumDeFotos/AlbumDeFotos/AlbumDeFotos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumDeFotos
+{
+    internal class Album
+    {
+        public double NumeroFotos { get; set; }
+        public double FotosPagina { get; set; }
+        public double Restante { get; set; }
+
+        public double Div { get; set; }
+
+        public string NumeroDePaginas()
+        {
+
+            Console.WriteLine("Digite quantas fotos você vai usar");
+            NumeroFotos = Convert.ToDouble(Console.ReadLine());
+            FotosPagina = 2;
+
+            Restante = NumeroFotos % FotosPagina;
+            Div = (NumeroFotos / FotosPagina) - 0.5;
+
+            if (Restante == 1)
+            {
+                return "Você pode colocar " + Div + " fotos no álbum e sobrará " + Restante + " foto(s)";
+            }
+            else
+            {
+                return "Você pode colocar " + NumeroFotos / FotosPagina + " foto por página";
+            }
+        }
+    }
+}
---
+++ Aula 11_04_23 - Exercícios/AlbumDeFotos/AlbumDeFotos/Program.cs
@@ -0,0 +1,18 @@
+﻿using AlbumDeFotos;
+
+namespace AlbumDeFotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando numero de paginas:");
+            Console.WriteLine("");
+
+            Album album = new Album();
+
+            Console.WriteLine(album.NumeroDePaginas());
+
+        }
+    }
+}
