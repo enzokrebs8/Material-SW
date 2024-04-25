c4798c7c622590f8ba2eaa4faf39d89592f7e74d
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-04-25 12:19:20 -0300

    Add files via upload

---
+++ ExemploConstrutor/ExemploConstrutor/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConstrutor
+{
+    internal class Cliente
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Senha { get; set; }
+
+        //public Cliente(string NomeConstruitor, int senhaConstrutor)
+        //{
+        //    Nome = NomeConstruitor;
+        //    Senha = senhaConstrutor;
+        //}
+
+        public Cliente()
+        {
+        }
+
+        public void MostraDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Senha: " + Senha);
+        }
+    }
+}
---
+++ ExemploConstrutor/ExemploConstrutor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo Contrutor de Classe");
+
+            Cliente novo_cliente = new Cliente();
+
+            novo_cliente.Nome = "Enzo";
+            novo_cliente.Email = "enzo@gmail.com";
+            novo_cliente.Senha = 150249;
+
+            novo_cliente.MostraDados();
+        }
+    }
+}
